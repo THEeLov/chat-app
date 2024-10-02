5f8d047195582cd83d3dc027b8a091cad8a2ce63
Filip Kozik <536410@mail.muni.cz>
2024-10-02 15:36:08 +0200

    feat: navbar added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,43 @@
-const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+import useAuthData from "../hooks/useAuthData";
+import ElectricBoltIcon from "@mui/icons-material/ElectricBolt";
+import UserMenu from "./UserMenu";
 
-export default Navbar
+const Navbar = () => {
+  const { user } = useAuthData();
+
+  return (
+    <AppBar position="sticky">
+      <Toolbar
+        sx={{ display: "flex", justifyContent: "space-between", gap: "1rem" }}
+      >
+        {/* LOGO */}
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <ElectricBoltIcon sx={{color: "#f2e146"}}/>
+          <Typography
+            variant="h6"
+            noWrap
+            sx={{
+              ml: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "#f2e045",
+              textDecoration: "none",
+              userSelect: "none"
+            }}
+          >
+            BOLT CHAT
+          </Typography>
+        </Box>
+
+        {/* ACCOUNT */}
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          {user && <UserMenu />}
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
---
+++ src/components/UserMenu.tsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import { Menu, MenuItem, IconButton, Avatar } from '@mui/material';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import { useNavigate } from 'react-router-dom';
+import useAuthData from '../hooks/useAuthData';
+
+const UserMenu = () => {
+  const { user, signOut } = useAuthData();
+  const navigate = useNavigate();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleSignOut = async () => {
+    try {
+      signOut();
+      navigate("/signin")
+    } catch (error) {
+      console.error("Failed to sign out:", error);
+    }
+    handleClose();
+  };
+
+  return (
+    <>
+      <IconButton
+        size="small"
+        aria-label="account of current user"
+        aria-controls="menu-appbar"
+        aria-haspopup="true"
+        onClick={handleMenu}
+        color="inherit"
+      >
+        {user?.profilePic ? (
+          <Avatar src={user.profilePic} alt="User Profile Picture" sx={{ width: 42, height: 42 }} />
+        ) : (
+          <AccountCircle />
+        )}
+      </IconButton>
+      <Menu
+        id="menu-appbar"
+        anchorEl={anchorEl}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        keepMounted
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={() => { handleClose(); navigate(`/user/${user!._id}`) }}>PROFILE</MenuItem>
+        <MenuItem onClick={() => { handleClose(); navigate(`/film-add`)}}>ADD FILMS</MenuItem>
+        <MenuItem onClick={handleSignOut}>SIGN OUT</MenuItem>
+      </Menu>
+    </>
+  );
+};
+
+export default UserMenu;
